8794d2296a9e532c3203ff30f7c1f8f0d9a78eb5
Tzvetomir Todorov <ttodorov@locqus.com>
2015-05-26 13:49:02 -0400

    Three Strikes and Refactor

--- lists/views.py
+++ lists/views.py
@@ -1,8 +1,17 @@
 from django.shortcuts import render
-"""from django.http import HttpResponse""" # No longer needed as we are now using the django render and not HttpResponse
+from django.http import HttpResponse # No longer needed as we are now using the django render and not HttpResponse
+									 # NOTE: Chapter 5 calls for import HttpResponse again
 
 # Create your views here.
 
 def home_page(request):
-	"""return HttpResponse('<html><title>To-Do lists</title></html>')""" #Instead of building our own HttpResponse, we now use the Django render function. 
-	return render(request, 'home.html')
+	return render(request, 'home.html', {
+		'new_item_text': request.POST.get('item_text', ''),
+	})
+
+	##if request.method == 'POST':
+		##return HttpResponse(request.POST['item_text'])
+	# return HttpResponse('<html><title>To-Do lists</title></html>') --> Instead of building our own HttpResponse, 
+	# 																     we now use the Django render function. 
+
+
