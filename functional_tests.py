43e02bdacb5f8923aa2025ae3f4ef882fabfd8fc
Tzvetomir Todorov <ttodorov@locqus.com>
2015-05-27 15:25:14 -0400

    Redirect after POST, and show all items in template

--- functional_tests.py
+++ functional_tests.py
@@ -3,111 +3,85 @@ from selenium.webdriver.common.keys import Keys
 import unittest
 import time
 
+
 class NewVisitorTest(unittest.TestCase):
 
-	def setUp(self):
-		self.browser = webdriver.Chrome()
-		self.browser.implicitly_wait(3)
+    def setUp(self):
+        self.browser = webdriver.Chrome()
+        self.browser.implicitly_wait(3)
 
-	def tearDown(self):
-		self.browser.quit()
+    def tearDown(self):
+        self.browser.quit()
 
-	def check_for_row_in_list_table(self, row_text):
-		table = self.browser.find_element_by_id('id_list_table')
-		rows = table.find_elements_by_tag_name('tr')
-		self.assertIn(row_text, [row.text for row in rows])
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
 
+    def test_can_start_a_list_and_retreive_it_later(self):
+        # Edith has heard about a cool new online to-do app. She goes
+        # to check out its homepage
+        self.browser.get('http://localhost:8000')
 
-	def test_can_start_a_list_and_retreive_it_later(self):
-		# Edith has heard about a cool new online to-do app. She goes
-		# to check out its homepage
-		self.browser.get('http://localhost:8000')
+        # She notices the page title and header mention to-do lists
+        self.assertIn('To-Do', self.browser.title)
 
-		# She notices the page title and header mention to-do lists
-		self.assertIn('To-Do', self.browser.title)
-			
-		# She is invited to enter a to-do item straight away
-		inputbox = self.browser.find_element_by_id('id_new_item')
-		self.assertEqual(
-				inputbox.get_attribute('placeholder'),
-				'Enter a to-do item'
-		)
+        # She is invited to enter a to-do item straight away
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'),
+            'Enter a to-do item'
+        )
 
-		# She types "Buy peacock feathers" into a text box (Edit's hobby
-		# is trying fly-fishing lures)
-		inputbox.send_keys('Buy peacock feathers')
+        # She types "Buy peacock feathers" into a text box (Edit's hobby
+        # is trying fly-fishing lures)
+        inputbox.send_keys('Buy peacock feathers')
 
-		# When she hits enter, the page updates, and now the page lists
-		# "1: Buy peacock feathers" as an item in a to-do list
-		inputbox.send_keys(Keys.ENTER)
-		self.check_for_row_in_list_table('1: Buy peacock feathers')
+        # When she hits enter, the page updates, and now the page lists
+        # "1: Buy peacock feathers" as an item in a to-do list
+        inputbox.send_keys(Keys.ENTER)
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
 
-		# table = self.browser.find_element_by_id('id_list_table')
-		# rows = table.find_elements_by_tag_name('tr')
-		# self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
-		
-		#<---This Part is done better above with one line as opposed to six---> 
-		# self.assertTrue(
-		# 	any(row.text == '1: Buy peacock feathers' for row in rows),
-		# 	"New to-do item did not appear in table -- its text was:\n%s" % (
-		# 		table.text,
-		# 	)
-		# )
-		#<-------------------------------------------------------------------->
-		
-		# There is still a text box inviting her to add another item. She
-		# enters "Use peacock feathers to make a fly" (Edit is very methodical)
-		inputbox = self.browser.find_element_by_id('id_new_item')
-		inputbox.send_keys('Use peacock feathers to make a fly')
-		inputbox.send_keys(Keys.ENTER)
+        # table = self.browser.find_element_by_id('id_list_table')
+        # rows = table.find_elements_by_tag_name('tr')
+        # self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
 
-		# The page updates again, and now shows both items on her list
-		self.check_for_row_in_list_table('1: Buy peacock feathers')
-		self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
+        #<---This Part is done better above with one line as opposed to six--->
+        # self.assertTrue(
+        # 	any(row.text == '1: Buy peacock feathers' for row in rows),
+        # 	"New to-do item did not appear in table -- its text was:\n%s" % (
+        # 		table.text,
+        # 	)
+        # )
+                #<-------------------------------------------------------------------->
 
-		# table = self.browser.find_element_by_id('id_list_table')
-		# rows = table.find_elements_by_tag_name('tr')
-		# self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
-		# self.assertIn(
-		# 	'2: Use peacock feathers to make a fly' ,
-		# 	 [row.text for row in rows]
-		# 	)
-	
-		# Edit wonders whether the site will remember he list. The she sees
-		# that the site has generated a unique URL for her -- there is some
-		# explanatory text to that effect.
-		self.fail('Finish the test!')
-		
-		# She visits that URL - her to-do list is still there.
+        # There is still a text box inviting her to add another item. She
+        # enters "Use peacock feathers to make a fly" (Edit is very methodical)
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Use peacock feathers to make a fly')
+        inputbox.send_keys(Keys.ENTER)
 
-		# Satisfied, she goes back to sleep
+        # The page updates again, and now shows both items on her list
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
+        self.check_for_row_in_list_table(
+            '2: Use peacock feathers to make a fly')
+
+        # table = self.browser.find_element_by_id('id_list_table')
+        # rows = table.find_elements_by_tag_name('tr')
+        # self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
+        # self.assertIn(
+        # 	'2: Use peacock feathers to make a fly' ,
+        # 	 [row.text for row in rows]
+        # 	)
+
+        # Edit wonders whether the site will remember he list. The she sees
+        # that the site has generated a unique URL for her -- there is some
+        # explanatory text to that effect.
+        self.fail('Finish the test!')
+
+        # She visits that URL - her to-do list is still there.
+
+        # Satisfied, she goes back to sleep
 
 if __name__ == '__main__':
-	unittest.main(warnings = 'ignore')
-
-
-
-
-
-
-
-	                                                                
-
-
-
-
-
-	                                
-	                                
-	                                                                
-	                                
-
-
-	                                                                
-
-	                                                                                                                                
-	                                                                
-
-
-
-	                                                                                                
+    unittest.main(warnings='ignore')
--- lists/views.py
+++ lists/views.py
@@ -1,17 +1,53 @@
-from django.shortcuts import render
-from django.http import HttpResponse # No longer needed as we are now using the django render and not HttpResponse
-									 # NOTE: Chapter 5 calls for import HttpResponse again
+from django.shortcuts import redirect, render
+# No longer needed as we are now using the django render and not HttpResponse
+from django.http import HttpResponse
+from lists.models import Item
+                                                                         # s
+                                                                         # NOTE:
+                                                                         # Chapter
+                                                                         # 5
+                                                                         # calls
+                                                                         # for
+                                                                         # import
+                                                                         # HttpResponse
+                                                                         # again
 
 # Create your views here.
 
+
 def home_page(request):
-	return render(request, 'home.html', {
-		'new_item_text': request.POST.get('item_text', ''),
-	})
+    if request.method == 'POST':
+        Item.objects.create(text=request.POST['item_text'])
+        return redirect('/')
 
-	##if request.method == 'POST':
-		##return HttpResponse(request.POST['item_text'])
-	# return HttpResponse('<html><title>To-Do lists</title></html>') --> Instead of building our own HttpResponse, 
-	# 																     we now use the Django render function. 
+    items = Item.objects.all()
+    return render(request, 'home.html', {'items': items})
 
+    # if request.method == 'POST':
+    # 	new_item_text = request.POST['item_text']
+    # 	Item.objects.create(text = new_item_text)
+    # else:
+    # 	new_item_text = ''
 
+    # return render(request, 'home.html', {
+    # 	'new_item_text': new_item_text,
+    # })
+
+    # return render(request, 'home.html', {
+    # 	'new_item_text': request.POST.get('item_text', ''),
+    # })
+
+    # item = Item()
+    # item.text = request.POST.get('item_text', '')
+    # item.save()
+
+    # if request.method == 'POST':
+    # 	Item.objects.create(text = request.POST['item_text'])
+    # 	return redirect('/')
+
+    # return render(request, 'home.html')
+
+    # if request.method == 'POST':
+        # return HttpResponse(request.POST['item_text'])
+    # return HttpResponse('<html><title>To-Do lists</title></html>') --> Instead of building our own HttpResponse,
+    # 																     we now use the Django render function.
