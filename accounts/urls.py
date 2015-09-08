a1dc89eae0493c2cc472bfba1d74772f979187e7
Tzvetomir Todorov <ttodorov@locqus.com>
2015-09-08 11:41:29 -0400

    Moved around static and tests folders. THIS commit.

--- accounts/urls.py
+++
@@ -1,6 +0,0 @@
-from django.conf.urls import url
-from accounts import views
-
-urlpatterns = [
-    url(r'^login$', views.persona_login, name='persona_login'),
-]
--- accounts/views.py
+++
@@ -1,6 +0,0 @@
-from django.contrib.auth import authenticate
-from django.http import HttpResponse
-
-def persona_login(request):
-    authenticate(assertion=request.POST['assertion'])
-    return HttpResponse()
