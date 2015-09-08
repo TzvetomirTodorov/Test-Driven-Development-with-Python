f1925f1b7041336e9c6861c0cf53c2d2b72b1752
Tzvetomir Todorov <ttodorov@locqus.com>
2015-09-08 16:45:31 -0400

    Added fix for Persona automatically logging back in after logout.

--- accounts/static/accounts.js
+++ accounts/static/accounts.js
@@ -2,6 +2,9 @@ var initialize = function (navigator, user, token, urls) {
     $('#id_login').on('click', function () {
         navigator.id.request();
     });
+    $('#id_logout').on('click', function() {
+        navigator.id.logout();
+    });
 
     navigator.id.watch({
         loggedInUser: user,
@@ -13,7 +16,14 @@ var initialize = function (navigator, user, token, urls) {
                 deferred.done(function () { window.location.reload(); })
                 deferred.fail(function () { navigator.id.logout(); });
         },
-        onlogout: function () {}
+        onlogout: function () {
+            var deferred = $.post(
+                urls.logout,
+                { csrfmiddlewaretoken: token }
+            )
+                deferred.done(function () { window.location.reload(); })
+                deferred.fail(function () { navigator.id.logout(); });
+        }
     });
 };
 
